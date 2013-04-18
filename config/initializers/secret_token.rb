587286bc083eb666bad2cc4c8e3a10ae7f4cab0c
Andreas Fast <andreas.fast@moove-it.com>
2013-04-18 01:01:57 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iepc::Application.config.secret_token = 'edf05bb88295b36238fc26176ccb692ae9093be4f8dcd8bb869e9cb77cb895418b24f3bd192c65e7ca9d3670d02271eb379148283245c2332550dd0b281ead0b'
