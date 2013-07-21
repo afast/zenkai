c88fd3dfb788da757a28a30df083c4ebe5332ff6
Gabriel Fagundez de los Reyes <gabriel.fagundez@moove-it.com>
2013-07-21 14:56:44 -0300

    Add seeds for user and project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,6 +5,13 @@ class Project < ActiveRecord::Base
 
   attr_accessible :abbreviation, :name, :url, :color, :type
 
+  def self.load_seeds
+    Project.create(name: 'Go Track', abbreviation: 'RSC', url: 'http://app.gotrackinc.com', color: '#0000FF', type: '')
+    Project.create(name: 'Fast Track Fleet', abbreviation: 'FTF', url: 'http://ftfleet.ftfleet.com', color: '#00FF00', type: '')
+    Project.create(name: 'Uplink GPS', abbreviation: 'UGPS', url: '', color: '#FF0000', type: '')
+    Project.create(name: 'Virtual Fleet Supervisor', abbreviation: 'VFS', url: 'http://login.virtualfleetsupervisor.com', color: '#FF00FF', type: '')
+  end
+
   def belongs_to_external_project?
     url.present?
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,38 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :email, :name
 
+  def self.load_seeds
+    if User.where(email: 'admin@moove-it.com').empty?
+      user = User.new(email: 'admin@moove-it.com', password: 'testingiepc', name: 'MooveIT IEPC Admin')
+      user.save!
+    end
+
+    if User.where(email: 'gabriel.fagundez@moove-it.com').empty?
+      user = User.new(email: 'gabriel.fagundez@moove-it.com', password: 'testingiepc', name: 'Gabriel Fagundez de los Reyes')
+      user.save!
+    end
+
+    if User.where(email: 'andreas.fast@moove-it.com').empty?
+      user = User.new(email: 'andreas.fast@moove-it.com', password: 'testingiepc', name: 'Andreas Fast')
+      user.save!
+    end
+
+    if User.where(email: 'santiago.piria@moove-it.com').empty?
+      user = User.new(email: 'santiago.piria@moove-it.com', password: 'testingiepc', name: 'Santiago Piria')
+      user.save!
+    end
+
+    if User.where(email: 'matias.nieves@moove-it.com').empty?
+      user = User.new(email: 'matias.nieves@moove-it.com', password: 'testingiepc', name: 'Matias Nieves')
+      user.save!
+    end
+
+    if User.where(email: 'virginia.rodriguez@moove-it.com').empty?
+      user = User.new(email: 'virginia.rodriguez@moove-it.com', password: 'testingiepc', name: 'Virginia Rodriguez')
+      user.save!
+    end
+  end
+
   def full_name
     name || email
   end
