96626e82283c71fca7e8d3bf80c7cb2ff3352d75
Gabriel Fagundez de los Reyes <gabriel.fagundez@moove-it.com>
2013-07-21 18:11:35 -0300

    Fix redirect after create or edit project. Fix initial style on root

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -44,7 +44,7 @@ class ProjectsController < ApplicationController
 
     respond_to do |format|
       if @project.save
-        format.html { redirect_to @project, notice: 'Project was successfully created.' }
+        format.html { redirect_to :projects, notice: name + ' was successfully created.' }
         format.json { render json: @project, status: :created, location: @project }
       else
         format.html { render action: "new" }
@@ -57,10 +57,11 @@ class ProjectsController < ApplicationController
   # PUT /projects/1.json
   def update
     @project = Project.find(params[:id])
+    name = @project.name
 
     respond_to do |format|
       if @project.update_attributes(params[:project])
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
+        format.html { redirect_to :projects, notice: name + ' was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -73,10 +74,11 @@ class ProjectsController < ApplicationController
   # DELETE /projects/1.json
   def destroy
     @project = Project.find(params[:id])
+    name = @project.name
     @project.destroy
 
     respond_to do |format|
-      format.html { redirect_to projects_url }
+      format.html { redirect_to projects_url, notice: name + ' was successfully deleted.' }
       format.json { head :no_content }
     end
   end
