89b2e33aa078e4592a02cc8264244fee8bdd01e0
Andreas Fast <andreas.fast@moove-it.com>
2013-10-01 22:51:08 -0300

    Enable asset compilation heroku

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Iepc
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.initialize_on_precompile = false
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
